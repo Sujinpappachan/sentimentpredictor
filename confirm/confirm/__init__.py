7278501ffc90bc7807c6a9b4cdac8cae8e494c56
Sujinpappachan <70098323+Sujinpappachan@users.noreply.github.com>
2021-01-20 17:18:18 +0530

    Update __init__.py

--- confirm/confirm/__init__.py
+++ confirm/confirm/__init__.py
@@ -18,8 +18,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] ='team3updates'
-app.config['MAIL_PASSWORD'] = 'kouxhxfcqkjgqpmn'
+app.config['MAIL_USERNAME'] =''
+app.config['MAIL_PASSWORD'] = ''
 mail = Mail(app)
 
 from confirm import routes
